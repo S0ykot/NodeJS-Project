01cd305eb0c75cee8ebe528772c5c41f38c16a1e
Khandakar Anim Hassan Adnan <khandakar.adnan21@gmail.com>
2020-03-15 03:08:13 +0600

    topic offer & details added

---
+++ models/AdminSubDomainModel.js
@@ -0,0 +1,56 @@
+var db = require('./db');
+
+module.exports ={
+	
+	getAllSubDomain:function(callback){
+		var sql = "select sub_domain.subDom_id,sub_domain.subDom_name,sub_domain.subDom_desc,thesis_type.type_name,domain_research.dom_name,faculty.faculty_fname,faculty.faculty_lname from sub_domain,thesis_type,domain_research,faculty where sub_domain.type_id=thesis_type.type_id and sub_domain.dom_id=domain_research.dom_id and sub_domain.fid=faculty.fid ";
+		db.getResult(sql, null, function(results){
+			callback(results);
+		});
+	},
+
+	getById: function(id, callback){
+		var sql = "select * from sub_domain where subDom_id=?";
+		db.getResult(sql, [id], function(result){
+			if(result){
+				callback(result);
+			}else{
+				callback(null);
+			}
+		});
+	},
+
+	addSubDomain: function(topic, callback){
+		var sql = "insert into sub_domain values(?,?,?,?,?,?)";
+		db.execute(sql, [null, topic.name, topic.description, topic.type, topic.domain, topic.supervisor], function(status){
+			if(status){
+				callback(true);
+			}else{
+				callback(false);
+			}
+		});
+	},
+
+	deleteTopic: function(id, callback){
+		var sql = "delete from sub_domain where subDom_id=?";
+		db.execute(sql, [id], function(status){
+			if(status){
+				callback(true);
+			}else{
+				callback(false);
+			}
+		});
+	},
+
+	updateSubDomain: function(topic, callback){
+		var sql = "update topic set subDom_name=?, subDom_desc=?, type_id=?, dom_id=?, fid=? where subDom_id=?";
+		db.execute(sql, [topic.name, topic.description, topic.type, topic.domain, topic.supervisor, topic.id], function(status){
+			if(status){
+				callback(true);
+			}else{
+				callback(false);
+			}
+		});
+	},
+
+}
